84cb5faab366938369099f1e60e5501a7af67411
Etienne Segonzac <etienne@segonzac.info>
2015-01-08 20:17:10 +0100

    Fake local install of the theme.

--- js/generation.js
+++ js/generation.js
@@ -1,6 +1,24 @@
 (function(exports) {
   'use strict';
 
+  function userStyle(theme) {
+    var str = ':root {\n';
+    Object.keys(theme.sections).forEach(function(sectionKey) {
+      var section = theme.sections[sectionKey];
+      Object.keys(section).forEach(function(key) {
+        str += key + ': ' + section[key] + ';\n';
+      });
+    });
+    str += '}'
+    return str;
+  }
+
+  function fakeInstall(css) {
+    var override = document.createElement('style');
+    override.innerHTML = css;
+    document.body.appendChild(override);
+  }
+
   exports.Generation = {
     installTheme: function(id) {
       return Storage.fetchTheme(id).then(function(theme) {
@@ -8,6 +26,9 @@
         console.log("| do some magic app generation |", theme.title);
         console.log("| do some magic app installation |", theme.title);
         console.log("| do some magic add-on enabling |", theme.title);
+
+        fakeInstall(userStyle(theme));
+
         return Promise.resolve();
       }).catch(function(error) {
         console.log(error);
